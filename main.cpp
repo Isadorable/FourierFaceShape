c923fef90426bae5e89f21bc5ed0b276082091ce
Izzy88 <isadora.sanna@gmail.com>
2017-08-04 19:05:37 -0700

    new changes

--- main.cpp
+++ main.cpp
@@ -30,11 +30,19 @@ typedef enum faceType{
 const int N_IMAGES = 15;
 const int candidate_points = 64;
 const int LANDS = 17;
+const int IM_PER_CLASS = 3;
+int mul = 1;
 
-Mat getSilhouette(frontal_face_detector detector, shape_predictor sp, string fileName);
+Mat getSilhouette(frontal_face_detector detector, shape_predictor sp, string fileName, std::vector<Point2d>& landmarks);
+Mat getSilhouetteDB(frontal_face_detector detector, shape_predictor sp, string fileName);
+
 std::vector<Point2d> getContour(Mat silhouette);
 std::vector<complex<float>> getFD(std::vector<Point2d> c1, Mat imc1);
 double compareFD(std::vector<complex<float>> fd1, std::vector<complex<float>> fd2);
+void extractAverageColour(std::vector<Point2d> contour, Mat inputIM);
+void registerFD(std::vector<complex<float>> imDBFD, faceType fType, ofstream& DBFDData);
+faceType getCategory(int index);
+std::vector<std::vector<complex<float>>> loadDB();
 
 
 int main(int argc, char *argv[])
@@ -43,52 +51,139 @@ int main(int argc, char *argv[])
     frontal_face_detector detector = get_frontal_face_detector();
     shape_predictor sp;
     CmShapeContext shapeContextEvaluator;
-    Mat imDB,myIM;
-    std::vector<Point2d> imDBpoints,myIMpoints;
+    Mat imDB,myIM, inputIM;
+    std::vector<Point2d> imDBpoints,myIMpoints, myLandmarks;
     std::vector<complex<float>> imDBFD, myIMFD;
     double score, sum;
     int c;
+    ofstream DBFDData;
+    std::vector<std::vector<complex<float>>> dataFromDB;
 
     deserialize("/home/isadora/Desktop/shape_predictor_68_face_landmarks.dat") >> sp;
 
     //LOAD IMAGES FROM THE DATASET AND COMPUTE FDs
-
-
-    myIM = getSilhouette(detector, sp, "/home/isadora/Desktop/finaltest/DBimgs/imtestD.jpg");
+    inputIM = imread("/home/isadora/Desktop/finaltest/DBimgs/imtest1.jpg");
+    myIM = getSilhouette(detector, sp, "/home/isadora/Desktop/finaltest/DBimgs/imtest1.jpg", myLandmarks);
     myIMpoints = getContour(myIM);
-    myIMFD = getFD(myIMpoints, myIM);
+    //myIMFD = getFD(myIMpoints, myIM);
 
 
-    Mat color;
-cv::cvtColor(myIM, color, cv::COLOR_GRAY2BGR);
-for(int j = 0; j < myIMpoints.size(); j++){
-    Point p = Point (myIMpoints.at(j).x,myIMpoints.at(j).y);
-    circle(color, p, 1, Scalar(0,0,255),1,8,0);
+    //VISUALISE LANDMARKS
+//    Mat color;
+//    cv::cvtColor(myIM, color, cv::COLOR_GRAY2BGR);
+//    for(int j = 0; j < myIMpoints.size(); j++){
+//        Point p = Point (myIMpoints.at(j).x,myIMpoints.at(j).y);
+//        circle(color, p, 1, Scalar(0,0,255),1,8,0);
+//    }
+    //imwrite( "/home/isadora/Desktop/finaltest/DBimgs/Gray_Image.jpg", color );
+
+
+    //GET FD INFO FROM DB AND REGISTER
+//    DBFDData.open ("/home/isadora/Desktop/finaltest/DBimgs/DBFDData.txt");
+//    for(unsigned int i = 0; i < N_IMAGES; i++){
+//        imDB = getSilhouetteDB(detector, sp, string("/home/isadora/Desktop/finaltest/DBimgs/imtest")+ to_string(i) + ".jpg");
+//        imDBpoints = getContour(imDB);
+//        imDBFD = getFD(imDBpoints, imDB);
+//        //Comment next line to skip registration to file
+//        registerFD(imDBFD, getCategory(i), DBFDData);
+
+//        //SCORES AND DISPLAY SCORES
+//        //score = shapeContextEvaluator.matchCost(imDBpoints, myIMpoints);
+//        score = compareFD(myIMFD, imDBFD);
+//        cout << "# " << i << " diff " << score << endl;
+
+//        if (c==3){sum = 0.0; c = 0;}
+//        sum += score;
+//        if (c==2)cout << "SUM " << sum << endl;
+//        c++;
+//    }
+//    DBFDData.close();
+
+
+    //compare FD with DB from file
+    dataFromDB = loadDB;
+
+
+    //COLOUR EXTRACTION
+    //face square under eyes uplips to avoid eyecolour and
+//    std::vector<Point2d> faceColourSelection;
+//    faceColourSelection.push_back(myLandmarks.at(1));
+//    faceColourSelection.push_back(myLandmarks.at(2));
+//    faceColourSelection.push_back(myLandmarks.at(3));
+//    faceColourSelection.push_back(myLandmarks.at(13));
+//    faceColourSelection.push_back(myLandmarks.at(14));
+//    faceColourSelection.push_back(myLandmarks.at(15));
+//    extractAverageColour(faceColourSelection, inputIM);
+//    //Mouth
+//    std::vector<Point2d> mouthColourSelection;
+//    for(unsigned int i = 48; i < 59; i++) mouthColourSelection.push_back(myLandmarks.at(i));
+//    extractAverageColour(mouthColourSelection, inputIM);
+
+//    //Eye (just one)
+//    std::vector<Point2d> eyeColourSelection;
+//    for(unsigned int i = 36; i < 41; i++) eyeColourSelection.push_back(myLandmarks.at(i));
 }
-        imwrite( "/home/isadora/Desktop/finaltest/DBimgs/Gray_Image.jpg", color );
 
-    for(unsigned int i = 0; i < N_IMAGES; i++){
-        imDB = getSilhouette(detector, sp, string("/home/isadora/Desktop/finaltest/DBimgs/imtest")+ to_string(i) + ".jpg");
-        imDBpoints = getContour(imDB);
-        imDBFD = getFD(imDBpoints, imDB);
+std::vector<std::vector<complex<float>>> loadDB(){
+    std::vector<std::vector<complex<float>>> globalFDVector;
+    std::vector<complex<float>> temp_vector;
+    ifstream DBFDData ("/home/isadora/Desktop/finaltest/DBimgs/DBFDData.txt");
+    std::complex<float> c;
 
-        //score = shapeContextEvaluator.matchCost(imDBpoints, myIMpoints);
-        score = compareFD(myIMFD, imDBFD);
-        cout << "# " << i << " diff " << score << endl;
-
-        if (c==3){sum = 0.0; c = 0;}
-        sum += score;
-        if (c==2)cout << "SUM " << sum << endl;
-        c++;
-
-
+    if (DBFDData.is_open())
+    {
+      while ( getline (DBFDData,line) )
+      {
+          std::stringstream stream(line);
+                  while(stream>>c)
+                  {
+                     vec.push_back(c);
+                  }
+      }
+      myfile.close();
     }
 
+}
 
-    //GET NEW IMAGE FD
-    //std::vector<complex<float>> FDMyImage = FDSingleImage(detector, sp);
-    //compareFourierDescriptors(FDDataset, FDMyImage);
+faceType getCategory(int index){
 
+    if(index == IM_PER_CLASS*mul) mul++;
+
+    return faceType(mul-1);
+}
+
+void registerFD(std::vector<complex<float>> imDBFD, faceType fType, ofstream& DBFDData){
+
+    for (int i = 0 ; i < imDBFD.size(); i++){
+        if(i == 0){
+            DBFDData << fType <<"\n";
+            DBFDData << imDBFD.at(i)<<"\n";
+        }else{
+        DBFDData << imDBFD.at(i)<<"\n";
+        }
+    }
+}
+
+void extractAverageColour(std::vector<Point2d> contour, Mat inputIM){
+    // Define a polygon
+    Point pts[1][contour.size()];
+    for(unsigned int i = 0; i < contour.size(); i++) pts[0][i] = Point (contour.at(i).x,contour.at(i).y);
+
+    const Point* points[1] = {pts[0]};
+    int npoints = contour.size();
+
+    // Create the mask with the polygon
+    Mat1b mask(inputIM.rows, inputIM.cols, uchar(0));
+    fillPoly(mask, points, &npoints, 1, Scalar(255));
+
+    // Compute the mean with the computed mask (BGR)
+    Scalar average = mean(inputIM, mask);
+
+
+    Mat bla;
+    inputIM.copyTo(bla, mask);
+    imwrite( "/home/isadora/Desktop/finaltest/DBimgs/Gray_Image.jpg", bla );
+    std::cout << "Average colour (RGB): "<< average[2] <<" "<< average[1] <<" "<< average[0] << std::endl;
 }
 
 double compareFD(std::vector<complex<float>> fd1, std::vector<complex<float>> fd2){
@@ -173,7 +268,7 @@ std::vector<Point2d> getContour(Mat gray){
     return filt_contours;
 }
 
-Mat getSilhouette(frontal_face_detector detector, shape_predictor sp, string fileName){
+Mat getSilhouette(frontal_face_detector detector, shape_predictor sp, string fileName, std::vector<Point2d>& landmarks){
     array2d<rgb_pixel> image;
 
     load_image(image, fileName);
@@ -191,6 +286,7 @@ Mat getSilhouette(frontal_face_detector detector, shape_predictor sp, string fil
          full_object_detection shape = sp(image, dets[j]);
          shapes.push_back(shape);
          face = shape;
+
      }
 
     for(int k = 0; k < LANDS; k++){
@@ -201,9 +297,9 @@ Mat getSilhouette(frontal_face_detector detector, shape_predictor sp, string fil
         }
     }
 
-    for(int l = 0; l <LANDS ; l++){
-        contourFD.push_back(Point2d (contour[0][l].x,contour[0][l].y));
-    }
+    for(int k = 0; k <68 ; k++) landmarks.push_back(Point2d(face.part(k).x(),face.part(k).y()));
+    for(int l = 0; l <LANDS ; l++) contourFD.push_back(Point2d (contour[0][l].x,contour[0][l].y));
+
 
     Mat imageCV;
     imageCV = imread(fileName);
@@ -225,3 +321,53 @@ Mat getSilhouette(frontal_face_detector detector, shape_predictor sp, string fil
 }
 
 
+Mat getSilhouetteDB(frontal_face_detector detector, shape_predictor sp, string fileName){
+    array2d<rgb_pixel> image;
+
+    load_image(image, fileName);
+    std::vector<dlib::rectangle> dets = detector(image);
+    Point contour[1][27];
+        full_object_detection face;
+    std::vector<Point2d> contourVector, filt_contours;
+        int cd = LANDS-1;
+        std::vector<Point2d> contourFD;
+
+    std::vector<full_object_detection> shapes;
+
+    for (unsigned long j = 0; j < dets.size(); ++j)
+     {
+         full_object_detection shape = sp(image, dets[j]);
+         shapes.push_back(shape);
+         face = shape;
+
+     }
+
+    for(int k = 0; k < LANDS; k++){
+        if (k <= 16) contour[0][k] = Point(face.part(k).x(),face.part(k).y());
+        else{
+            contour[0][cd] = Point(face.part(k).x(),face.part(k).y());
+            cd--;
+        }
+    }
+
+    for(int l = 0; l <LANDS ; l++) contourFD.push_back(Point2d (contour[0][l].x,contour[0][l].y));
+
+
+    Mat imageCV;
+    imageCV = imread(fileName);
+    const Point* ppt[1] = { contour[0] };
+
+    int npt[] = { LANDS };
+    fillPoly( imageCV, ppt, npt, 1, Scalar( 0, 0, 0 ), 1 );
+
+    Mat mask(imageCV.rows, imageCV.cols, CV_8UC3, Scalar(255,255,255));
+    fillPoly( mask, ppt, npt, 1, Scalar( 0, 0, 0 ), 1 );
+
+    Mat gray;
+    cvtColor(mask, gray, CV_BGR2GRAY);
+    threshold(gray, gray, 1, 255, CV_THRESH_BINARY | CV_THRESH_OTSU);
+    bitwise_not (gray, gray);
+
+    //WE HAVE THE SILHOUETTE SAVED IN <MASK>
+    return gray;
+}
